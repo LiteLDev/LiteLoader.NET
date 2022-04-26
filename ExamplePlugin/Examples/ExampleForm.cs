0a7713458819d72cccf3c0bccc8178e2eab0bc67
Pd233 <minecraftpd233@163.com>
2022-04-26 20:03:13 +0800

    摸鱼

---
+++ ExamplePlugin/Examples/ExampleForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//using Form Namesapce
+using LLNET.Form;
+using LLNET.Logger;
+
+namespace ExamplePlugin.Examples
+{
+    public class ExampleForm : IExample
+    {
+        static readonly Logger logger = new("ExampleForm");
+        public void Execute()
+        {
+            //Create a simple form
+            SimpleForm simpleForm = new("ExampleForm", "A Example Form");
+
+            //Append an simple button
+            simpleForm.append(new Button("Button1"));
+
+            //Set form callback
+            simpleForm.callback = (pl, val) =>
+            {
+                logger.info.WriteLine("PlayerNAme:{0}", pl.Name);
+                logger.info.WriteLine("buttonVal:{0}", val);
+            };
+
+            //Send form to player
+            MC.Level.GetAllPlayers().ForEach(player =>
+            {
+                simpleForm.sendTo(player);
+
+                int i;
+            });
+        }
+    }
+}
