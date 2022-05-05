11511f78bfd3e4bc15d5b2981c68da6211dcc79a
Pd233 <minecraftpd233@163.com>
2022-05-05 15:34:04 +0800

    Update 2.2.2

--- ExamplePlugin/Examples/ExampleForm.cs
+++ ExamplePlugin/Examples/ExampleForm.cs
@@ -1,6 +1,7 @@
 ﻿//using Form Namesapce
 using LLNET.Form;
 using LLNET.Logger;
+using LLNET.DynamicCommand;
 using MC;
 
 namespace ExamplePlugin.Examples
@@ -8,25 +9,85 @@ namespace ExamplePlugin.Examples
     public class ExampleForm : IExample
     {
         private static readonly Logger logger = new("ExampleForm");
-        public void Execute()
-        {
-            //Create a simple form
-            SimpleForm simpleForm = new("ExampleForm", "A Example Form");
 
+        //Create a simple form
+        private static readonly SimpleForm simpleForm = new("ExampleForm", "A Example Form");
+
+        private static readonly CustomForm customForm = new("CustomFormTitle");
+
+        static ExampleForm()
+        {
             //Append an simple button
             simpleForm.Append(new("Button1"));
 
             //Set form callback
-            simpleForm.callback = (pl, val) =>
+            simpleForm.Callback = (pl, val) =>
             {
                 logger.info.WriteLine("PlayerName:{0}", pl.Name);
                 logger.info.WriteLine("buttonVal:{0}", val);
             };
 
-            //Send form to player
-            Level.GetAllPlayers().ForEach(player =>
+
+
+
+
+            customForm.Append(new Label("LabelName", "LabelTitle"));
+            customForm.Append(new Input("InputName", "InputTitle"));
+            customForm.Append(new Toggle("ToggleName", "ToggleTitle"));
+            customForm.Append(new Slider("SliderName", "SliderTitle", 0, 100));
+            customForm.Append(new Dropdown("DropdownName", "DropdownTitle", new()
             {
-                simpleForm.SendTo(player);
+                "DropDown-1",
+                "DropDown-2",
+                "DropDown-3"
+            }));
+            customForm.Append(new StepSlider("StepSlider", "TestStepSlider", new()
+            {
+                "StepSlider-1",
+                "StepSlider-2",
+                "StepSlider-3"
+            }));
+
+            customForm.Callback = (pl, val) =>
+            {
+                var input = (Input)val["InputName"];
+
+                logger.info.WriteLine("FormInput:{0}", input?.Value);
+            };
+
+            CreateCommand();
+        }
+
+
+        public void Execute()
+        {
+
+        }
+
+        private static void CreateCommand()
+        {
+
+            var command = DynamicCommand.CreateCommand("testformcommand", "command description", CommandPermissionLevel.Any);
+
+            var simpleFormParam = command.SetEnum("Form Type", new() { "simpleform", "customform" });
+
+            command.Mandatory("FormEnum", DynamicCommand.ParameterType.Enum, simpleFormParam??"");
+
+            command.SetCallback((cmd, origin, output, results) =>
+            {
+                switch ((string)results["FormEnum"].Get())
+                {
+                    case "simpleform":
+                        {
+                            simpleForm.SendTo(origin.Player);
+                        }
+                        break;
+                    case "customform":
+                        {
+                            customForm.SendTo(origin.Player);
+                        }
+                        break;
+                }
             });
         }
     }
