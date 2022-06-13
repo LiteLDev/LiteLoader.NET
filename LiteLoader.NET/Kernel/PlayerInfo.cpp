53a05a64b3001b9746d1fd04d1e8eb9df4f64795
Pd233 <minecraftpd233@163.com>
2022-06-13 09:02:58 +0800

    Update PlayerInfo.cpp

--- LiteLoader.NET/Kernel/PlayerInfo.cpp
+++ LiteLoader.NET/Kernel/PlayerInfo.cpp
@@ -3,23 +3,23 @@
 namespace LLNET::PlayerInfo {
     inline bool PlayerInfoAPI::Insert(System::String^ name, System::String^ xuid, System::String^ uuid)
     {
-        return ::PlayerInfo::insert(marshalString<Encoding::E_UTF8>(name), marshalString<Encoding::E_UTF8>(xuid), marshalString<Encoding::E_UTF8>(uuid));
+        return ::PlayerInfo::insert(marshalString(name), marshalString(xuid), marshalString(uuid));
     }
     inline System::String^ PlayerInfoAPI::GetXuid(System::String^ name)
     {
-        return marshalString<Encoding::E_UTF8>(::PlayerInfo::getXuid(marshalString<Encoding::E_UTF8>(name)));
+        return marshalString(::PlayerInfo::getXuid(marshalString(name)));
     }
     inline System::String^ PlayerInfoAPI::GetUUID(System::String^ name)
     {
-        return marshalString<Encoding::E_UTF8>(::PlayerInfo::getUUID(marshalString<Encoding::E_UTF8>(name)));
+        return marshalString(::PlayerInfo::getUUID(marshalString(name)));
     }
     inline System::String^ PlayerInfoAPI::FromXuid(System::String^ name)
     {
-        return marshalString<Encoding::E_UTF8>(::PlayerInfo::fromXuid(marshalString<Encoding::E_UTF8>(name)));
+        return marshalString(::PlayerInfo::fromXuid(marshalString(name)));
     }
     inline System::String^ PlayerInfoAPI::FromUUID(System::String^ name)
     {
-        return marshalString<Encoding::E_UTF8>(::PlayerInfo::fromUUID(marshalString<Encoding::E_UTF8>(name)));
+        return marshalString(::PlayerInfo::fromUUID(marshalString(name)));
     }
     inline void PlayerInfoAPI::ForEachInfo(forEachInfoDelegate^ callback)
     {
@@ -34,6 +34,6 @@ namespace LLNET::PlayerInfo {
     }
     bool PlayerInfoAPI::forEachInfoCallback(std::string_view name, std::string_view xuid, std::string_view uuid)
     {
-        return _callback(marshalString<Encoding::E_UTF8>((std::string)name), marshalString<Encoding::E_UTF8>((std::string)xuid), marshalString<Encoding::E_UTF8>((std::string)uuid));
+        return _callback(marshalString((std::string)name), marshalString((std::string)xuid), marshalString((std::string)uuid));
     }
 }
