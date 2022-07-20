8b4ff2656312df36b82c8fd5a3d6e018fbf104f9
S3v3N <s3v3nicee@gmail.com>
2022-07-20 18:54:33 +0300

    Add ColorFormat api

---
+++ LiteLoader.NET/Header/MC/ColorFormat.hpp
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include "Types.hpp"
+#include "MC/ColorFormat.hpp"
+
+namespace MC
+{
+public
+ref class ColorFormat abstract sealed
+{
+public:
+    static initonly String^ Aqua = marshalString(::ColorFormat::AQUA);
+    static initonly String^ Black = marshalString(::ColorFormat::BLACK);
+    static initonly String^ Blue = marshalString(::ColorFormat::BLUE);
+    static initonly String^ Bold = marshalString(::ColorFormat::BOLD);
+    static initonly String^ DarkAqua = marshalString(::ColorFormat::DARK_AQUA);
+    static initonly String^ DarkBlue = marshalString(::ColorFormat::DARK_BLUE);
+    static initonly String^ DarkGray = marshalString(::ColorFormat::DARK_GRAY);
+    static initonly String^ DarkGreen = marshalString(::ColorFormat::DARK_GREEN);
+    static initonly String^ DarkPurple = marshalString(::ColorFormat::DARK_PURPLE);
+    static initonly String^ DarkRed = marshalString(::ColorFormat::DARK_RED);
+    static initonly String^ Escape = marshalString(::ColorFormat::ESCAPE);
+    static initonly String^ Gold = marshalString(::ColorFormat::GOLD);
+    static initonly String^ Gray = marshalString(::ColorFormat::GRAY);
+    static initonly String^ Green = marshalString(::ColorFormat::GREEN);
+    static initonly String^ Italic = marshalString(::ColorFormat::ITALIC);
+    static initonly String^ LightPurple = marshalString(::ColorFormat::LIGHT_PURPLE);
+    static initonly String^ MinecoinGold = marshalString(::ColorFormat::MINECOIN_GOLD);
+    static initonly String^ Obfuscated = marshalString(::ColorFormat::OBFUSCATED);
+    static initonly String^ Red = marshalString(::ColorFormat::RED);
+    static initonly String^ Reset = marshalString(::ColorFormat::RESET);
+    static initonly String^ White = marshalString(::ColorFormat::WHITE);
+    static initonly String^ Yellow = marshalString(::ColorFormat::YELLOW);
+
+    static String^ ConsoleCodeFromColorCode(String^ mcCode);
+    static String^ NearestColorCodeFromConsoleCode(String^ consoleCode);
+    static String^ ConvertToMc(String^ str);
+    static String^ ConvertToConsole(String^ str, bool keepColor);
+    static String^ ConvertToConsole(String^ str);
+    static String^ RemoveColorCode(String^ str);
+
+
+};
+}
---
+++ LiteLoader.NET/Kernel/MC/ColorFormat.cpp
@@ -0,0 +1,34 @@
+﻿#include "../../Header/MC/ColorFormat.hpp"
+
+namespace MC
+{
+    String^ ColorFormat::ConsoleCodeFromColorCode(String^ mcCode)
+    {
+        return marshalString(::ColorFormat::consoleCodeFromColorCode(marshalString(mcCode)));
+    }
+
+    String^ ColorFormat::NearestColorCodeFromConsoleCode(String^ consoleCode)
+    {
+        return marshalString(::ColorFormat::nearestColorCodeFromConsoleCode(marshalString(consoleCode)));
+    }
+
+    String^ ColorFormat::ConvertToMc(String^ str)
+    {
+        return marshalString(::ColorFormat::convertToMc(marshalString(str)));
+    }
+
+    String^ ColorFormat::ConvertToConsole(String^ str, bool keepColor)
+    {
+        return marshalString(::ColorFormat::convertToColsole(marshalString(str), keepColor));
+    }
+
+    String^ ColorFormat::ConvertToConsole(String^ str)
+    {
+        return marshalString(::ColorFormat::convertToColsole(marshalString(str)));
+    }
+
+    String^ ColorFormat::RemoveColorCode(String^ str)
+    {
+        return marshalString(::ColorFormat::removeColorCode(marshalString(str)));
+    }
+}
