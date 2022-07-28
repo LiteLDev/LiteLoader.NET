37fe720ddffd9cf5e8d75d4cbd8f252fe08bface
S3v3N <s3v3nicee@gmail.com>
2022-07-28 17:22:57 +0300

    Upd UserEntityIdentifierComponent

--- LiteLoader.NET/Header/MC/UserEntityIdentifierComponent.hpp
+++ LiteLoader.NET/Header/MC/UserEntityIdentifierComponent.hpp
@@ -1,12 +1,23 @@
 #pragma once
 #include <MC/UserEntityIdentifierComponent.hpp>
+#include "NetworkIdentifier.hpp"
+#include "Certificate.hpp"
 #include "Types.hpp"
+
 namespace MC
 {
-public
-ref class UserEntityIdentifierComponent : ClassTemplate<UserEntityIdentifierComponent, ::UserEntityIdentifierComponent>
-{
-public:
-    __ctor(UserEntityIdentifierComponent, ::UserEntityIdentifierComponent);
-};
+    public
+        ref class UserEntityIdentifierComponent : ClassTemplate<
+            UserEntityIdentifierComponent, ::UserEntityIdentifierComponent>
+    {
+    public:
+        __ctor(UserEntityIdentifierComponent, ::UserEntityIdentifierComponent);
+
+        property NetworkIdentifier^ NetworkId { NetworkIdentifier^ get(); }
+        property System::Byte ClientSubId { System::Byte get(); }
+        property Mce::UUID Uuid { Mce::UUID get(); }
+        property String^ PlayFabId { String^ get(); }
+        property Certificate^ Certificate { MC::Certificate^ get(); }
+        property bool IsPrimaryClient { bool get(); }
+    };
 } // namespace MC
