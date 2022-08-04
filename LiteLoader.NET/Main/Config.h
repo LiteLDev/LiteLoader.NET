652eea14eb707ebea39f031c8123985169f0d65c
Pd233 <minecraftpd233@163.com>
2022-08-04 12:01:02 +0800

    Init

--- LiteLoader.NET/Main/Config.h
+++ LiteLoader.NET/Main/Config.h
@@ -12,13 +12,32 @@
 
 #define LLNET_PLUGINS_LOAD_DIR "plugins"
 
+#define LLNET_LIBRARY_DIR_DOTNETONLY "plugins/lib/dotnet"
+
+#define LLNET_RUNTIME_CONFIG_JSON_PATH "plugins/LiteLoader/LiteLoader.NET.runtimeconfig.json"
+
+#define LLNET_LOADER_PATH "plugins/LiteLoader/LiteLoader.NET"
+
+#define LLNET_MANAGED_ENTRY_CLASS "LLNET.__Entry"
+
+#define LLNET_MANAGED_ENTRY_METHOD "InitAndLoadPlugins"
+
+
+
 #define LITELOADER_CONFIG_FILE "plugins/LiteLoader/LiteLoader.json"
 
 #define LITELOADER_LIBRARY_DIR "plugins/lib"
 
-#define LITELOADER_LIBRARY_DIR_DOTNETONLY "plugins/lib/dotnet"
-
 #define LITELOADER_VERSION LL::getLoaderVersion()
+
+
+
+#define DOTNET_RUNTIME_DIR "plugins/lib/dotnet/runtime"
+
+#define DOTNET_RUNTINE_HOSTFXR_DLL_PATH "plugins/lib/dotnet/runtime/hostfxr.dll"
+
+#define DOTNET_NETHOST_DLL_PATH "plugins/lib/dotnet/nethost.dll"
+
 
 //#define INCLUDE_MCAPI
 
--- LiteLoader.NET/Main/Loader.cpp
+++ LiteLoader.NET/Main/Loader.cpp
@@ -5,32 +5,200 @@
 #include "IPluginInitializer.hpp"
 
 
+
+#pragma managed
+
+//managed
 Assembly^ OnAssemblyResolve(System::Object^ sender, System::ResolveEventArgs^ args);
 
-void Init(Logger& logger);
+//managed
+void Init();
 
 //inline void InitializePluginHandler(std::filesystem::path const& path, Assembly^ Asm);
 
+//managed
 bool LoadByDefaultEntry(Logger& logger, Assembly^ Asm);
 
+//managed
 bool LoadByCustomEntry(Logger& logger, Assembly^ Asm);
 
+//managed
 void LoadPlugins(std::vector<std::filesystem::path> const& assemblyPaths, Logger& logger);
 
+//managed
 List<String^>^ ParsePluginLibraryPath(Assembly^ Asm);
+
+
+namespace LLNET {
+	public ref class __Entry
+	{
+	public:
+		static void InitAndLoadPlugins(void* pLogger, void* std_vector_assemblies)
+		{
+			Init();
+			LoadPlugins(
+				*reinterpret_cast<std::vector<std::filesystem::path>*>(std_vector_assemblies),
+				*reinterpret_cast<::Logger*>(pLogger));
+		}
+	};
+}
+
+void __entry(void* pLogger, void* std_vector_assemblies) { LLNET::__Entry::InitAndLoadPlugins(pLogger, std_vector_assemblies); }
 
 #pragma unmanaged
 #include "Global.hpp"
 #include <Utils/Hash.h>
+#include <sstream>
 
 #pragma unmanaged
+#include "../Extra/nethost.h"
+#include "../Extra/hostfxr.h"
+#include "../Extra/coreclr_delegates.h"
+
+std::vector<std::filesystem::path> GetAllAssemblies();
+
 void LoadMain()
 {
 	Logger logger(LLNET_LOADER_NAME);
 
-	Init(logger);
+	auto nethost_dll = LoadLibrary(TEXT(DOTNET_NETHOST_DLL_PATH));
+
+	if (nethost_dll == nullptr)
+	{
+		logger.error("Cannot load nethost.dll!");
+		throw std::exception();
+	}
+
+	auto get_hostfxr_path_fn = reinterpret_cast<
+		int(__stdcall*)(char_t*, size_t*, const struct get_hostfxr_parameters*)>(
+			GetProcAddress(nethost_dll, "get_hostfxr_path"));
+
+	if (get_hostfxr_path_fn == nullptr)
+	{
+		logger.error("Cannot get exported function from <nethost.dll>!");
+		throw std::exception();
+	}
+
+	char_t buffer[MAX_PATH];
+	size_t buffer_size = sizeof(buffer) / sizeof(char_t);
+	int rc = get_hostfxr_path_fn(buffer, &buffer_size, nullptr);
+
+	FreeLibrary(nethost_dll);
+
+
+
+	if (rc == 0)
+	{
+		auto assemblies = GetAllAssemblies();
+		__entry(&logger, &assemblies);
+	}
+	else
+	{
+		if (!std::filesystem::exists(DOTNET_RUNTIME_DIR))
+		{
+			logger.error("Cannot find local .NET runtime path!");
+			throw std::exception();
+		}
+		else
+		{
+			auto hostfxr_dll = LoadLibrary(TEXT(DOTNET_RUNTINE_HOSTFXR_DLL_PATH));
+
+			if (hostfxr_dll == nullptr)
+			{
+				logger.error("Cannot load hostfxr.dll!");
+				throw std::exception();
+			}
+
+			auto init_fptr = reinterpret_cast<hostfxr_initialize_for_runtime_config_fn>(
+				GetProcAddress(hostfxr_dll, "hostfxr_initialize_for_runtime_config"));
+
+			auto get_delegate_fptr = reinterpret_cast<hostfxr_get_runtime_delegate_fn>(
+				GetProcAddress(hostfxr_dll, "hostfxr_get_runtime_delegate"));
+
+			auto close_fptr = reinterpret_cast<hostfxr_close_fn>(
+				GetProcAddress(hostfxr_dll, "hostfxr_close"));
+
+			if (init_fptr == nullptr || get_delegate_fptr == nullptr || close_fptr == nullptr)
+			{
+				logger.error("Cannot get exported function from <hostfxr.dll>!");
+				throw std::exception();
+			}
+
+
+
+			load_assembly_and_get_function_pointer_fn load_assembly_and_get_function_pointer = nullptr;
+			hostfxr_handle cxt = nullptr;
+
+			int rc = init_fptr(TEXT(LLNET_RUNTIME_CONFIG_JSON_PATH), nullptr, &cxt);
+
+			constexpr auto to_hex_string = [](int i)->std::string
+			{
+				std::stringstream ioss;
+				string s_temp;
+				ioss << std::setiosflags(std::ios::uppercase) << std::hex << std::showbase << i;
+				ioss >> s_temp;
+				return s_temp;
+			};
+
+			if (rc != 0 || cxt == nullptr)
+			{
+				logger.error("Init failed: {}", to_hex_string(rc));
+				close_fptr(cxt);
+				throw std::exception();
+			}
+
+			rc = get_delegate_fptr(
+				cxt,
+				hdt_load_assembly_and_get_function_pointer,
+				reinterpret_cast<void**>(&load_assembly_and_get_function_pointer));
+
+			if (rc != 0 || load_assembly_and_get_function_pointer == nullptr)
+			{
+				logger.error("Get delegate failed: {}", to_hex_string(rc));
+				close_fptr(cxt);
+				throw std::exception();
+			}
+
+
+
+			component_entry_point_fn initAndLoadPlugins_fptr = nullptr;
+
+			rc = load_assembly_and_get_function_pointer(
+				TEXT(LLNET_LOADER_PATH),
+				TEXT(LLNET_MANAGED_ENTRY_CLASS),
+				TEXT(LLNET_MANAGED_ENTRY_METHOD),
+				nullptr,
+				nullptr,
+				reinterpret_cast<void**>(&initAndLoadPlugins_fptr));
+
+
+			if (rc != 0 || initAndLoadPlugins_fptr == nullptr)
+			{
+				logger.error("Load LiteLoader.NET failed: {}", to_hex_string(rc));
+				close_fptr(cxt);
+				throw std::exception();
+			}
+
+
+
+			auto assemblies = GetAllAssemblies();
+
+			struct managed_entry_args
+			{
+				void* pLogger;
+				void* std_vector_assemblies;
+			}
+			args{ &logger,&assemblies };
+
+			initAndLoadPlugins_fptr(&args, sizeof(managed_entry_args));
+		}
+	}
 
 	logger.info("Loading .NET plugins...");
+}
+
+std::vector<std::filesystem::path> GetAllAssemblies()
+{
 	std::filesystem::directory_iterator files(LLNET_PLUGINS_LOAD_DIR);
 	std::vector<std::filesystem::path> assemblies;
 	for (auto& file : files)
@@ -43,16 +211,15 @@ void LoadMain()
 			assemblies.emplace_back(filePath);
 		}
 	}
-
-	LoadPlugins(assemblies, logger);
+	return assemblies;
 }
 
 #pragma managed
-extern void InitEvents(Logger& logger);
+extern void InitEvents();
 
-void Init(Logger& logger)
+void Init()
 {
-	InitEvents(logger);
+	InitEvents();
 	System::AppDomain::CurrentDomain->AssemblyResolve += gcnew System::ResolveEventHandler(&OnAssemblyResolve);
 	auto LLNET_Asm = Assembly::GetExecutingAssembly();
 	GlobalClass::ManagedModuleHandler->TryAdd(LLNET_Asm, IntPtr(::LL::getPlugin(LLNET_LOADER_NAME)->handle));
@@ -72,8 +239,8 @@ Assembly^ OnAssemblyResolve(System::Object^ sender, System::ResolveEventArgs^ ar
 	{
 		return Assembly::LoadFrom(llLibPath);
 	}
-	
-	auto llLibPath_dotnet = Path::Combine(LITELOADER_LIBRARY_DIR_DOTNETONLY, assemblyName.Name + ".dll");
+
+	auto llLibPath_dotnet = Path::Combine(LLNET_LIBRARY_DIR_DOTNETONLY, assemblyName.Name + ".dll");
 	if (File::Exists(llLibPath_dotnet))
 	{
 		return Assembly::LoadFrom(llLibPath_dotnet);
