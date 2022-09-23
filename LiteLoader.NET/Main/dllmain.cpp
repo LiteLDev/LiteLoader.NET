8f7d21859fca0dcd51bd30191383055b96eaaf59
LazuliKao <lazulikao@nit.zju.edu.cn>
2022-09-23 21:07:50 +0800

    Update dllmain.cpp
    lib to lowercase

--- LiteLoader.NET/Main/dllmain.cpp
+++ LiteLoader.NET/Main/dllmain.cpp
@@ -1,7 +1,7 @@
-#pragma comment(lib, "../SDK/Lib/bedrock_server_api.lib")
-#pragma comment(lib, "../SDK/Lib/bedrock_server_var.lib")
-#pragma comment(lib, "../SDK/Lib/SymDBHelper.lib")
-#pragma comment(lib, "../SDK/Lib/LiteLoader.lib")
+#pragma comment(lib, "../SDK/lib/bedrock_server_api.lib")
+#pragma comment(lib, "../SDK/lib/bedrock_server_var.lib")
+#pragma comment(lib, "../SDK/lib/SymDBHelper.lib")
+#pragma comment(lib, "../SDK/lib/LiteLoader.lib")
 
 #pragma unmanaged
 #include <windows.h>
