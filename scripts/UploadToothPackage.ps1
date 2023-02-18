a07f6a6808d6cb171ced98212feea4fb91b23e72
星梦StarsDream <starsdream00@icloud.com>
2023-02-18 16:23:09 +0800

    Fix script

--- scripts/UploadToothPackage.ps1
+++ scripts/UploadToothPackage.ps1
@@ -14,7 +14,7 @@ Write-Output -InputObject ""
 
 git clone $LLNET_TOOTH_REMOTE_PATH
 
-Set-Location -Path ".\tooth-repo"
+Set-Location -Path ".\LLDotNET"
 git fetch --all
 git reset --hard origin/main
 git checkout main
@@ -26,19 +26,19 @@ Write-Output -InputObject ""
 
 # remove refs directory in tooth repo
 Write-Output -InputObject  "[INFO] Removing LiteLoader dir"
-Remove-Item -Path ".\tooth-repo\LiteLoader\*" -Recurse
+Remove-Item -Path ".\LLDotNET\LiteLoader\*" -Recurse
 Write-Output -InputObject  "[INFO] Removing lib dir"
-Remove-Item -Path ".\tooth-repo\lib\*" -Recurse
+Remove-Item -Path ".\LLDotNET\lib\*" -Recurse
 eWrite-Output -InputObject "[INFO] Removing tooth.json"
-Remove-Item -Path ".\tooth-repo\tooth.json" -Recurse
+Remove-Item -Path ".\LLDotNET\tooth.json" -Recurse
 
 # copy all to tooth repo
-Copy-Item -Path ".\x64\Release\LiteLoader.NET.dll" -Destination ".\tooth-repo\LiteLoader\LiteLoader.NET.dll"
-Copy-Item -Path ".\x64\Release\LiteLoader.NET.runtimeconfig.json" -Destination ".\tooth-repo\LiteLoader\LiteLoader.NET.runtimeconfig.json"
-Copy-Item -Path ".\x64\Release\Ijwhost.json" -Destination ".\tooth-repo\lib\Ijwhost.json"
-Copy-Item -Path ".\output\RELEASE\tooth.json" -Destination ".\tooth-repo\tooth.json"
+Copy-Item -Path ".\x64\Release\LiteLoader.NET.dll" -Destination ".\LLDotNET\LiteLoader\LiteLoader.NET.dll"
+Copy-Item -Path ".\x64\Release\LiteLoader.NET.runtimeconfig.json" -Destination ".\LLDotNET\LiteLoader\LiteLoader.NET.runtimeconfig.json"
+Copy-Item -Path ".\x64\Release\Ijwhost.json" -Destination ".\LLDotNET\lib\Ijwhost.json"
+Copy-Item -Path ".\output\RELEASE\tooth.json" -Destination ".\LLDotNET\tooth.json"
 
-Set-Location -Path ".\tooth-repo"
+Set-Location -Path ".\LLDotNET"
 git status . -s | Set-Variable -Name "LLNET_TOOTH_NOW_STATUS"
 if ($LLNET_TOOTH_NOW_STATUS -ne "") {
     Write-Output "[INFO] Modified files found."
@@ -48,9 +48,9 @@ if ($LLNET_TOOTH_NOW_STATUS -ne "") {
     Write-Output -InputObject ""
     Write-Output "[INFO] Pushing to origin..."
     Write-Output -InputObject ""
-    git push "https://$env:USERNAME`:$env:REPO_KEY@github.com/LiteLDev/tooth-repo.git" main
+    git push "https://$env:USERNAME`:$env:REPO_KEY@github.com/Tooth-Hub/LLDotNET.git" main
     git tag $LLNET_NOW_TAG
-    git push --tags "https://$env:USERNAME`:$env:REPO_KEY@github.com/LiteLDev/tooth-repo.git" main
+    git push --tags "https://$env:USERNAME`:$env:REPO_KEY@github.com/Tooth-Hub/LLDotNET.git" main
     Set-Location "..\"
     Write-Output -InputObject ""
     Write-Output -InputObject "[INFO] Upload finished."
